a200439fcced02b112c5ab09d41bc77de8345c7f
BenoitMorel <benomorel@gmail.com>
2018-02-20 12:07:20 +0100

    raxml_runner: full implementation (not tested)

--- scripts/raxml_runner.py
+++ scripts/raxml_runner.py
@@ -25,6 +25,9 @@ def print_help():
 def get_first_run_output(output_dir):
   return os.path.join(output_dir, "first_run")
 
+def get_second_run_output(output_dir):
+  return os.path.join(output_dir, "second_run")
+
 def run_multiraxml(command_filename, output_dir, ranks):
   command = []
   command.append("mpirun")
@@ -45,13 +48,54 @@ def build_first_command(raxml_exec_dir, fasta_files, output_dir, options, ranks)
   with open(first_command_file, "w") as writer:
     for fasta in fasta_files:
       base = os.path.splitext(os.path.basename(fasta))[0]
-      writer.write("c_" + base + " nompi 1 1 " + raxml)
+      writer.write("first_" + base + " nompi 1 1 " + raxml)
       writer.write(" --parse ")
       writer.write( " --msa " + fasta + " " + options[:-1])
       writer.write(" --prefix " + os.path.join(first_run_output_dir, base))
       writer.write(" --threads 1 ")
       writer.write("\n")
   return first_command_file
+
+def sites_to_maxcores(sites):
+  if sites == 0:
+    return 0
+  return 1 << ((sites // 500) -1).bit_length()
+
+def parse_msa_info(log_file):
+  result = [0, 0]
+  lines = open(log_file).readlines()
+  for line in lines:
+    if "Alignment comprises" in line:
+      result[0] = int(line.split(" ")[5])
+    if "taxa" in line:
+      result[1] = int(line.split(" ")[4])
+  result[0] = sites_to_maxcores(result[0])
+  return result
+
+def build_second_command(raxml_exec_dir, fasta_files, output_dir, options, ranks):
+  second_command_file = os.path.join(output_dir, "second_command.txt")
+  first_run_output_dir = get_first_run_output(output_dir)
+  second_run_output_dir = get_second_run_output(output_dir)
+  os.makedirs(second_run_output_dir)
+  with open(second_command_file, "w") as writer:
+    for fasta in fasta_files:
+      base = os.path.splitext(os.path.basename(fasta))[0]
+      raxml = os.path.join(raxml_exec_dir, "raxml-ng-mpi")
+      first_run_log = os.path.join(os.path.join(first_run_output_dir, base + ".raxml.log"))
+      parse_result = parse_msa_info(first_run_log)
+      cores = str(parse_result[0])
+      taxa = str(parse_result[1])
+      if (cores == "0" or taxa == "0"):
+        print("error with fasta " + fasta + ", skipping")
+        continue
+      writer.write("second_" + base + " mpi ")
+      writer.write(cores + " " + taxa + " " + raxml)
+      writer.write(" --msa " + fasta + " " + options[:-1])
+      writer.write(" --prefix " + os.path.join(second_run_output_dir, base))
+      writer.write(" --threads 1 ")
+      writer.write("\n")
+  return second_command_file
+
 
 def main_raxml_runner(raxml_exec_dir, fasta_dir, output_dir, options_file, ranks):
   check_ranks(ranks)
@@ -61,6 +105,9 @@ def main_raxml_runner(raxml_exec_dir, fasta_dir, output_dir, options_file, ranks
   options = open(options_file, "r").readlines()[0]
   first_command_file = build_first_command(raxml_exec_dir, fasta_files, output_dir, options, ranks)
   run_multiraxml(first_command_file, output_dir, ranks)
+  second_command_file = build_second_command(raxml_exec_dir, fasta_files, output_dir, options, ranks)
+  run_multiraxml(second_command_file, output_dir, ranks)
+
 
 if (len(sys.argv) != 6):
     print_help()
