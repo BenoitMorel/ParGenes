5fcb42e7ee9eea1f759762685ecfb2bf638c55fb
BenoitMorel <benomorel@gmail.com>
2018-05-04 14:41:43 +0200

    Add forgotten files

---
+++ mpi-scheduler/src/onecore_implem/OneCoreImplem.cpp
@@ -0,0 +1,207 @@
+#include "OneCoreImplem.hpp"
+#include <cstdlib>
+#include <stdio.h>
+#include <sys/wait.h>
+
+namespace MPIScheduler {
+  
+const int SIGNAL_JOB = 2;
+const int SIGNAL_TERMINATE = 3;
+
+const int TAG_END_JOB = 1;
+const int TAG_START_JOB = 2;
+const int TAG_MASTER_SIGNAL = 4;
+
+const int MSG_SIZE_END_JOB = 3;
+  
+
+OneCoreSlave::~OneCoreSlave()
+{
+
+}
+
+int OneCoreSlave::main_core_slave(int argc, char **argv)
+{
+  SchedulerArgumentsParser arg(argc, argv);
+  _execPath = arg.library;
+  _outputDir = arg.outputDir;
+  _commands = CommandsContainer(arg.commandsFilename);
+  MPI_Comm_size(MPI_COMM_WORLD, &_masterRank);
+  _masterRank -= 1;
+  while (true) {
+    int signal;
+    MPI_Status status;
+    MPI_Recv(&signal, 1, MPI_INT, _masterRank, TAG_MASTER_SIGNAL, MPI_COMM_WORLD, &status);
+    if (SIGNAL_JOB == signal) {
+      treatJobSlave();         
+    } else if (SIGNAL_TERMINATE == signal) {
+      cout << "received terminate" << endl; 
+      MPI_Finalize();
+      break;
+    }
+  }
+  return 0;
+}
+
+void OneCoreSlave::treatJobSlave()
+{
+  MPI_Status status;
+  const int maxCommandSize = 200;
+  char command[maxCommandSize];
+  MPI_Recv(command, maxCommandSize, MPI_CHAR, _masterRank, TAG_START_JOB, MPI_COMM_WORLD, &status);
+  Timer timer;
+  int startingTime = _globalTimer.getElapsedMs();
+  int jobResult = doWork(_commands.getCommand(string(command)), _outputDir);
+  int elapsedMS = timer.getElapsedMs();
+  int endJobMsg[MSG_SIZE_END_JOB];
+  endJobMsg[0] = jobResult;
+  endJobMsg[1] = startingTime;
+  endJobMsg[2] = elapsedMS;
+  MPI_Send(endJobMsg, MSG_SIZE_END_JOB, MPI_INT, _masterRank, TAG_END_JOB, MPI_COMM_WORLD);
+}
+
+
+int systemCall(const string &command, const string &outputFile)
+{
+  int result = 0;
+  char buf[BUFSIZ];
+  FILE *ptr, *file;
+  file = fopen(outputFile.c_str(), "w");
+  if ((ptr = popen(command.c_str(), "r")) != NULL) {
+    while (fgets(buf, BUFSIZ, ptr) != NULL) {
+      fprintf(file, "%s", buf);
+    }
+    result = pclose(ptr);
+  }
+  fclose(file);
+  return result;
+}
+
+int OneCoreSlave::doWork(const CommandPtr command, 
+    const string &outputDir) 
+{
+  string logsFile = Common::joinPaths(outputDir, "per_job_logs", command->getId() + "_out.txt");
+  string runningFile = Common::joinPaths(outputDir, "running_jobs", command->getId());
+  ofstream os(runningFile);
+  os << logsFile << endl;
+  os.close();
+  const vector<string> &args  = command->getArgs();
+  string systemCommand = _execPath;
+  for (auto &arg: args) {
+    systemCommand = systemCommand + " " + arg;
+  }
+  int result = systemCall(systemCommand, logsFile);
+  remove(runningFile.c_str());
+  return result;
+}
+
+OneCoreInstance::OneCoreInstance(const string &outputDir, 
+      int rank, 
+      CommandPtr command):
+  Instance(command, rank, 1)
+{
+}
+
+
+bool OneCoreInstance::execute(InstancePtr self)
+{
+  _beginTime = Common::getTime();
+  int signal = SIGNAL_JOB;
+  MPI_Send(&signal, 1, MPI_INT, self->getStartingRank(), TAG_MASTER_SIGNAL, MPI_COMM_WORLD); 
+  MPI_Send((char *)self->getId().c_str(), self->getId().size() + 1, MPI_CHAR, self->getStartingRank(), TAG_START_JOB, MPI_COMM_WORLD);
+  return true;
+}
+
+void OneCoreInstance::writeSVGStatistics(SVGDrawer &drawer, const Time &initialTime)
+{
+}
+
+OneCoreRanksAllocator::OneCoreRanksAllocator(int availableRanks,
+    const string &outputDir):
+  _cores(availableRanks - 1),
+  _outputDir(outputDir)
+{
+  Common::makedir(Common::joinPaths(outputDir, "per_job_logs"));
+  Common::makedir(Common::joinPaths(outputDir, "running_jobs"));
+  for (int i = 0; i < _cores; ++i) {
+    _availableCores.push(i);
+  }
+}
+
+bool OneCoreRanksAllocator::ranksAvailable()
+{
+  return !_availableCores.empty();
+}
+ 
+bool OneCoreRanksAllocator::allRanksAvailable()
+{
+  return _cores == _availableCores.size();
+}
+  
+void OneCoreRanksAllocator::terminate()
+{
+  while (_availableCores.size()) {
+    cout << "send terminate to " << _availableCores.front() << endl;
+    int signal = SIGNAL_TERMINATE;
+    MPI_Send(&signal, 1, MPI_INT, _availableCores.front(), TAG_MASTER_SIGNAL, MPI_COMM_WORLD);
+    _availableCores.pop();
+  }
+}
+  
+InstancePtr OneCoreRanksAllocator::allocateRanks(int requestedRanks, 
+      CommandPtr command)
+{
+  int rank = _availableCores.front();
+  _availableCores.pop();
+  InstancePtr instance(new OneCoreInstance(_outputDir,
+    rank,
+    command));
+  _rankToInstances[rank] = instance;
+  return instance;
+}
+
+void OneCoreRanksAllocator::freeRanks(InstancePtr instance)
+{
+  _availableCores.push(instance->getStartingRank());
+}
+
+vector<InstancePtr> OneCoreRanksAllocator::checkFinishedInstances()
+{
+  vector<InstancePtr> finished;
+  Timer t;
+  while (true) {
+    MPI_Status status;
+    int flag;
+    MPI_Iprobe(MPI_ANY_SOURCE, TAG_END_JOB, MPI_COMM_WORLD, &flag, &status); 
+    if (!flag) {
+      break;
+    }
+    int source = status.MPI_SOURCE;
+    int endJobMsg[MSG_SIZE_END_JOB];
+    MPI_Recv(endJobMsg, MSG_SIZE_END_JOB, MPI_INT, source,  
+        TAG_END_JOB, MPI_COMM_WORLD, &status);
+    auto foundInstance = _rankToInstances.find(source);
+    if (foundInstance == _rankToInstances.end()) {
+      cerr << "Error: did not find the instance started on rank " << source << endl;
+      break;
+    }
+    auto instance = static_pointer_cast<OneCoreInstance>(foundInstance->second);
+    instance->setStartingElapsedMS(endJobMsg[1]);
+    instance->setElapsedMs(endJobMsg[2]);
+    if (endJobMsg[0]) {
+      cerr << "Warning, command " << instance->getId() 
+           << " failed with return code " << endJobMsg[0] << endl;
+    }
+    finished.push_back(instance);
+    //_rankToInstances.erase(source);
+
+  }
+  if (finished.size()) {
+    cout << "Check finished elapsed time " << t.getElapsedMs()  << " for " << finished.size() << " elements" << endl;
+  }
+  return finished;
+}
+
+
+
+}
---
+++ mpi-scheduler/src/onecore_implem/OneCoreImplem.hpp
@@ -0,0 +1,71 @@
+#ifndef ONECORE_IMPLEM_HPP
+#define ONECORE_IMPLEM_HPP
+
+#include "../Command.hpp"
+#include <queue>
+
+
+namespace MPIScheduler {
+
+class OneCoreSlave {
+public:
+  OneCoreSlave() {}
+  ~OneCoreSlave();
+  int main_core_slave(int argc, char **argv);
+
+  int doWork(const CommandPtr command, 
+    const string &outputDir) ;
+private:
+  void treatJobSlave();
+  string _execPath;
+  string _outputDir;
+  int _masterRank;
+  Timer _globalTimer;
+  CommandsContainer _commands;
+};
+
+/*
+ *  This allocator only returns single ranks
+ */
+class OneCoreRanksAllocator: public RanksAllocator {
+public:
+  // available threads must be a power of 2
+  OneCoreRanksAllocator(int availableRanks, 
+      const string &outoutDir);
+  virtual ~OneCoreRanksAllocator() {}
+  virtual bool ranksAvailable();
+  virtual bool allRanksAvailable();
+  virtual InstancePtr allocateRanks(int requestedRanks, 
+      CommandPtr command);
+  virtual void freeRanks(InstancePtr instance);
+  virtual vector<InstancePtr> checkFinishedInstances();
+  virtual void terminate();
+private:
+  int _cores;
+  queue<int> _availableCores;
+  string _outputDir;
+  //map<string, InstancePtr> _runningInstances;
+  map<int, InstancePtr> _rankToInstances;
+
+};
+
+class OneCoreInstance: public Instance {
+public:
+  OneCoreInstance(const string &outputDir, 
+      int rank, 
+      CommandPtr command);
+
+  virtual ~OneCoreInstance() {}
+  virtual bool execute(InstancePtr self);
+  virtual void writeSVGStatistics(SVGDrawer &drawer, const Time &initialTime); 
+  void setStartingElapsedMS(int starting) {_startingElapsedMS = starting;}
+private:
+  int _startingElapsedMS;
+  string _outputDir;
+};
+
+} // namespace MPIScheduler
+
+
+#endif
+
