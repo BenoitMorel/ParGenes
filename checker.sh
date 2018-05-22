79ca4c975697adcba05d534eced5d9780b6b9f25
BenoitMorel <benomorel@gmail.com>
2018-05-22 10:38:33 +0200

    Add a checker script to check that all the binaries exist

---
+++ checker.sh
@@ -0,0 +1,16 @@
+
+check_file_exists()
+  if [ -f $1 ]
+  then
+    echo "$1 exists... OK!"
+  else
+    echo "ERROR: $1 does not exist"
+    exit 1
+  fi
+
+check_file_exists "raxml-ng/bin/raxml-ng-mpi.so"
+check_file_exists "modeltest/build/src/modeltest-ng-mpi.so"
+check_file_exists "mpi-scheduler/build/mpi-scheduler"
+
+echo "End of checks... No error detected!"
+
