44eb6687a867550392934f5d4fa6900e9910ec2e
BenoitMorel <benomorel@gmail.com>
2018-09-04 21:50:20 +0200

    Add forgotter file

---
+++ pargenes/logger.py
@@ -0,0 +1,30 @@
+import logging
+import commons
+import sys
+import time
+from datetime import timedelta
+
+pargenes_logger = logging.getLogger('pargenes')
+
+def init_logger(op):
+  global pargenes_logger
+  logs = commons.get_log_file(op.output_dir, "pargenes_logs")
+  pargenes_logger.addHandler(logging.FileHandler(logs))
+  pargenes_logger.addHandler(logging.StreamHandler(sys.stdout))
+
+def info(msg):
+  global pargenes_logger
+  pargenes_logger.error(msg)
+
+def warning(msg):
+  info("[Warning] " + msg)
+
+def error(msg):
+  info("[Error] " + msg)
+
+
+def timed_log(initial_time, msg):
+  """ Print the time elapsed from initial_time and the msg """ 
+  elapsed = time.time() - initial_time
+  info("[" + str(timedelta(seconds = int(elapsed))) + "] " + msg)
+
