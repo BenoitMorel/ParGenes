61befb896d30623c42850e34bcb2654c6727a44a
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2018-12-10 13:43:12 +0100

    Logs: print the starting time, and add elapsed time to all warning and error messages

--- pargenes/logger.py
+++ pargenes/logger.py
@@ -6,25 +6,34 @@ from datetime import timedelta
 
 pargenes_logger = logging.getLogger("pargenes")
 pargenes_logger.addHandler(logging.StreamHandler(sys.stdout))
+pargenes_logger_start = time.time()
+
+
 
 def init_logger(output_dir):
   global pargenes_logger
   logs = commons.get_log_file(output_dir, "pargenes_logs")
   pargenes_logger.addHandler(logging.FileHandler(logs))
 
-def info(msg):
+
+def log(msg):
   global pargenes_logger
   pargenes_logger.error(msg)
 
+def timed_log(msg, flag = ""):
+  """ Print the time elapsed from initial_time and the msg """ 
+  global pargenes_logger_start
+  elapsed = time.time() - pargenes_logger_start
+  log(flag + "[" + str(timedelta(seconds = int(elapsed))) + "] " + msg)
+
+def info(msg):
+  log(msg)
+
+
 def warning(msg):
-  info("[Warning] " + msg)
+  global pargenes_logger_start
+  timed_log(msg, "[Warning] " )
 
 def error(msg):
-  info("[Error] " + msg)
-
-
-def timed_log(initial_time, msg):
-  """ Print the time elapsed from initial_time and the msg """ 
-  elapsed = time.time() - initial_time
-  info("[" + str(timedelta(seconds = int(elapsed))) + "] " + msg)
+  timed_log(msg, "[Error] ")
 
--- pargenes/pargenes.py
+++ pargenes/pargenes.py
@@ -13,11 +13,15 @@ import shutil
 import logger
 import astral
 import report
+import datetime
+
 
 def print_header(args):
   logger.info("########################")
   logger.info("#    PARGENES v1.0.1   #")
   logger.info("########################")
+  logger.info("")
+  print(datetime.datetime.now())
   logger.info("ParGenes was called as follow:")
   logger.info(" ".join(args))
   logger.info("")
@@ -29,8 +33,8 @@ def print_stats(op):
   failed_commands = os.path.join(op.output_dir, "failed_commands.txt")
   if (os.path.isfile(failed_commands)):
     failed_number = len(open(failed_commands).readlines())
-    logger.info("[Warning] Total number of jobs that failed: " + str(failed_number))
-    logger.info("[Warning] For a detailed list, see " + failed_commands)
+    logger.warning("Total number of jobs that failed: " + str(failed_number))
+    logger.warning("For a detailed list, see " + failed_commands)
 
 def main_raxml_runner(args, op): 
   """ Run pargenes from the parsed arguments op """
@@ -44,7 +48,7 @@ def main_raxml_runner(args, op):
   logger.init_logger(op.output_dir)
   print_header(args)
   msas = None
-  logger.timed_log(start, "end of MSAs initializations")
+  logger.timed_log("end of MSAs initializations")
   scriptdir = os.path.dirname(os.path.realpath(__file__))
   modeltest_run_path = os.path.join(output_dir, "modeltest_run")
   raxml_run_path = os.path.join(output_dir, "mlsearch_run")
@@ -59,42 +63,42 @@ def main_raxml_runner(args, op):
     raxml.run_parsing_step(msas, raxml_library, op.scheduler, os.path.join(output_dir, "parse_run"), op.cores, op)
     raxml.analyse_parsed_msas(msas, op)
     checkpoint.write_checkpoint(output_dir, 1)
-    logger.timed_log(start, "end of parsing mpi-scheduler run")
+    logger.timed_log("end of parsing mpi-scheduler run")
   else:
     msas = raxml.load_msas(op)
   if (op.dry_run):
     logger.info("End of the dry run. Exiting")
     return 0
-  logger.timed_log(start, "end of anlysing parsing results") 
+  logger.timed_log("end of anlysing parsing results") 
   if (op.use_modeltest):
     if (checkpoint_index < 2):
       modeltest.run(msas, output_dir, modeltest_library, modeltest_run_path, op)
-      logger.timed_log(start, "end of modeltest mpi-scheduler run")
+      logger.timed_log("end of modeltest mpi-scheduler run")
       modeltest.parse_modeltest_results(op.modeltest_criteria, msas, output_dir)
-      logger.timed_log(start, "end of parsing  modeltest results")
+      logger.timed_log("end of parsing  modeltest results")
       # then recompute the binary MSA files to put the correct model, and reevaluate the MSA sizes with the new models
       shutil.move(os.path.join(output_dir, "parse_run"), os.path.join(output_dir, "old_parse_run"))
       raxml.run_parsing_step(msas, raxml_library, op.scheduler, os.path.join(output_dir, "parse_run"), op.cores, op)
       raxml.analyse_parsed_msas(msas, op)
-      logger.timed_log(start, "end of the second parsing step") 
+      logger.timed_log("end of the second parsing step") 
       checkpoint.write_checkpoint(output_dir, 2)
   if (checkpoint_index < 3):
     raxml.run(msas, op.random_starting_trees, op.parsimony_starting_trees, op.bootstraps, raxml_library, op.scheduler, raxml_run_path, op.cores, op)
-    logger.timed_log(start, "end of mlsearch mpi-scheduler run")
+    logger.timed_log("end of mlsearch mpi-scheduler run")
     checkpoint.write_checkpoint(output_dir, 3)
   if (op.random_starting_trees + op.parsimony_starting_trees > 1):
     if (checkpoint_index < 4):
       raxml.select_best_ml_tree(msas, op)
-      logger.timed_log(start, "end of selecting the best ML tree")
+      logger.timed_log("end of selecting the best ML tree")
       checkpoint.write_checkpoint(output_dir, 4)
   if (op.bootstraps != 0):
     if (checkpoint_index < 5):
       bootstraps.concatenate_bootstraps(output_dir, min(16, op.cores))
-      logger.timed_log(start, "end of bootstraps concatenation")
+      logger.timed_log("end of bootstraps concatenation")
       checkpoint.write_checkpoint(output_dir, 5)
     if (checkpoint_index < 6):
       bootstraps.run(output_dir, raxml_library, op.scheduler, os.path.join(output_dir, "supports_run"), op.cores, op)
-      logger.timed_log(start, "end of supports mpi-scheduler run")
+      logger.timed_log("end of supports mpi-scheduler run")
       checkpoint.write_checkpoint(output_dir, 6)
   if (op.use_astral):
     if (checkpoint_index < 7):
@@ -119,7 +123,7 @@ def run_pargenes(args):
     logger.info("[Error] " + str(type(inst)) + " " + str(inst)) 
     report.report_and_exit(op.output_dir, 1)
   end = time.time()
-  logger.timed_log(start, "END OF THE RUN OF " + os.path.basename(__file__))
+  logger.timed_log("END OF THE RUN OF " + os.path.basename(__file__))
   if (ret != 0):
     logger.info("Something went wrong, please check the logs") 
 
