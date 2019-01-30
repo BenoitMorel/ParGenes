374bbdff744a70a6b9dad6c4c83c6da9e4b54eb3
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2019-01-30 12:15:50 +0100

    Add a gitpull script to pull and recursively update submodules

---
+++ gitpull.sh
@@ -0,0 +1,21 @@
+git pull
+
+git submodule update --init
+
+cd MPIScheduler
+git submodule update --init
+cd ..
+
+cd raxml-ng
+git submodule update --init
+cd libs/pll-modules
+git submodule update --init
+cd ../../../
+
+cd modeltest
+git submodule update --init
+cd libs/pll-modules
+git submodule update --init
+cd ../../../
+
+
