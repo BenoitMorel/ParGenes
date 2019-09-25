04f79f3639876fefda762d0b7cda29451df8f985
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2019-09-25 13:51:37 +0200

    Fix macos bug

--- pargenes/pargenes-hpc.py
+++ pargenes/pargenes-hpc.py
@@ -7,7 +7,7 @@ args = sys.argv
 args.append("--scheduler")
 if ("darwin" in platform.system().lower()):
   print("Warning: ParGenes hpc mode is not supported on MacOS. ParGenes will run in hpc-debug mode instead. For more information, read the wiki page about running ParGenes on MacOS")
-  args.append("one-core")
+  args.append("onecore")
 else:
   args.append("split")
 pargenescore.run_pargenes(args)
