f5a97bd4296b92d65f7c3ba467ba1ba9d4411b10
BenoitMorel <benomorel@gmail.com>
2019-11-07 10:53:36 +0100

    Simplify the gitpull.h script (thanks Pierre)

--- gitpull.sh
+++ gitpull.sh
@@ -1,21 +1,3 @@
 git pull
-
-git submodule update --init
-
-cd MPIScheduler
-git submodule update --init
-cd ..
-
-cd raxml-ng
-git submodule update --init
-cd libs/pll-modules
-git submodule update --init
-cd ../../../
-
-cd modeltest
-git submodule update --init
-cd libs/pll-modules
-git submodule update --init
-cd ../../../
-
+git submodule update --init --recursive
 
