3957a849d0319015da307769a21c908395396da0
BenoitMorel <benomorel@gmail.com>
2020-03-10 13:44:10 +0100

    Remove check in pargenes tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -100,8 +100,8 @@ def run_command(command, run_name):
   sys.stdout.write("[" + run_name + "]: ")
   sys.stdout.flush()
   with open(logs, "wb", 0) as out:
-    subprocess.check_call(shlex.split(command))
-  print("Success! (" + str(int((time.time() - start_time))) + "s)")
+    subprocess.call(shlex.split(command))
+  #print("Success! (" + str(int((time.time() - start_time))) + "s)")
 
 def test_help():
   command = pargenes_scripts[0] + " -h"
