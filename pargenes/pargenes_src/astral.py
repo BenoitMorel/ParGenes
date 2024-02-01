d2f6443abf062712cd6ff2669a7f8a588eaa4c2d
nylander <Johan.Nylander@nrm.se>
2024-02-01 17:23:21 +0100

    Polish blank lines and trailing spaces. Minor edit in error output

--- pargenes/pargenes_src/astral.py
+++ pargenes/pargenes_src/astral.py
@@ -22,7 +22,7 @@ def extract_gene_trees_support(pargenes_dir, gene_trees_filename):
           writer.write(treat_newick(reader.read()))
         count += 1
   logger.info("ParGenes/Astral: " + str(count) + " gene trees (with support values) were found in ParGenes output directory")
-  
+
 def extract_gene_trees_ml(pargenes_dir, gene_trees_filename):
   results = os.path.join(pargenes_dir, "mlsearch_run", "results")
   count = 0
@@ -36,10 +36,9 @@ def extract_gene_trees_ml(pargenes_dir, gene_trees_filename):
       except:
         continue
   logger.info("ParGenes/Astral: " + str(count) + " trees were found in ParGenes output directory")
-  
-  
+
 def extract_gene_trees(pargenes_dir, gene_trees_filename):
-  """ Get all ParGenes ML gene trees and store then in gene_trees_filename 
+  """ Get all ParGenes ML gene trees and store then in gene_trees_filename
   """
   with_supports = os.path.isdir(os.path.join(pargenes_dir, "supports_run"))
   results = ""
@@ -55,9 +54,8 @@ def get_additional_parameters(parameters_file):
   if (len(astral_options) != 0):
     return astral_options[0][:-1].split(" ")
   else:
-    return [] 
-  
-  
+    return []
+
 def run_astral(pargenes_dir, astral_jar, parameters_file):
   """ Run astral on the ParGenes ML trees
   - pargenes_dir     (str):        pargenes output directory
@@ -85,7 +83,7 @@ def run_astral(pargenes_dir, astral_jar, parameters_file):
   command +="-jar "
   command += astral_jar + " "
   command += "-i " + gene_trees + " "
-  command += "-o " + astral_output + " " 
+  command += "-o " + astral_output + " "
   for arg in astral_args:
     command += arg + " "
   sys.stderr.write(command)
@@ -99,7 +97,6 @@ def run_astral(pargenes_dir, astral_jar, parameters_file):
     logger.error("Astral execution failed")
     report.report_and_exit(pargenes_dir, 1)
 
-
 def run_astral_pargenes(astral_jar, op):
   run_astral(op.output_dir, astral_jar, op.astral_global_parameters)
 
@@ -107,7 +104,7 @@ if (__name__ == '__main__'):
   if (len(sys.argv) != 2 and len(sys.argv) != 3):
     print("Error: syntax is:")
     print("python astral.py pargenes_dir [astral_parameters_file]")
-    print("astral_parameters_file is optional and should contain additional parameters to pass to astral call)")
+    print("astral_parameters_file is optional and should contain additional parameters to pass to astral call.")
     exit(1)
   pargenes_dir = sys.argv[1]
   parameters_file = None
